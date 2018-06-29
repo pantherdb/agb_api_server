0ff5c5b5d292ba72827f52268b6a7c7f6d6961b6
Xiaosong Huang <xhuang310@gatech.edu>
2018-06-28 19:04:36 -0700

    init

---
+++ controllers/genelist.js
@@ -0,0 +1,127 @@
+//Require the express package and use express.Router()
+const express = require('express');
+const router = express.Router();
+const genelist = require('../models/list');
+const species = require('../models/species');
+
+//GET HTTP method to /genelist
+router.get('/',(req,res) => {
+    genelist.getAllLists((err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+
+    }
+    });
+});
+
+router.get('/species',(req,res) => {
+    species.getAllSpecies((err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+
+    }
+    });
+});
+
+/* router.get('/species/:species',(req,res) => {
+    var species = req.params.species;
+    genelist.getListsBySpecies(species, (err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+
+    }
+    });
+}); */
+
+router.get('/species/:species',(req,res) => {
+    var spe = req.params.species;
+    species.getAllGenesBySpecies(spe, (err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+
+    }
+    });
+});
+
+router.get('/gene/:ptn',(req,res) => {
+    var ptn = req.params.ptn;
+    genelist.getGeneByPtn(ptn, (err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+
+    }
+    });
+});
+
+/* router.get('/genes',(req,res) => {
+    //var ptn = req.params.ptn;
+    genelist.getAllgenes((err, lists)=> {
+        if(err) {
+            res.json({success:false, message: `Failed to load all lists. Error: ${err}`});
+        }
+        else {
+            res.write(JSON.stringify({success: true, lists:lists},null,2));
+            res.end();
+
+    }
+    });
+}); */
+
+//POST HTTP method to /genelist
+
+router.post('/', (req,res,next) => {
+    console.log(req.body);
+    var newList = new genelist({
+        ptn: req.body.ptn,
+        name: req.body.name,
+        species: req.body.species,
+        sequence: req.body.sequence
+    });
+    genelist.addList(newList,(err) => {
+        if(err) {
+            res.json({success: false, message: `Failed to create a new list. Error: ${err}`});
+
+        }
+        else
+            res.json({success:true, message: "Added successfully."});
+
+    });
+});
+
+//DELETE HTTP method to /genelist. Here, we pass in a params which is the object id.
+router.delete('/:id', (req,res,next)=> {
+    //access the parameter which is the id of the item to be deleted
+      var id = req.params.id;
+    //Call the model method deleteListById
+      list.deleteListById(id,(err,list) => {
+          if(err) {
+              res.json({success:false, message: `Failed to delete the list. Error: ${err}`});
+          }
+          else if(list) {
+              res.json({success:true, message: "Deleted successfully"});
+          }
+          else
+              res.json({success:false});
+      })
+  });
+  module.exports = router;
---
+++ models/species.js
@@ -0,0 +1,38 @@
+//Require mongoose package
+const mongoose = require('mongoose');
+
+//Define GenelistSchema
+
+const SpeciesSchema = mongoose.Schema({
+    species: [String]
+});
+
+const SpeciesGenesSchema = mongoose.Schema({
+    short_name: String,
+    taxon_id: String,
+    long_name: String,
+    genes: [
+        {
+            ptn:String,
+            name:String,
+            event:String,
+            pthr:String,
+            proxy_org:String,
+            proxy_gene:String
+
+        }
+    ]
+});
+
+const Species = module.exports = mongoose.model('Species', SpeciesSchema );
+
+const SpeciesGenes = module.exports = mongoose.model('species_genes', SpeciesGenesSchema );
+
+//GeneList.find() returns all the lists
+module.exports.getAllSpecies = (callback) => {
+    SpeciesGenes.find({}).select({ "short_name": 1, "long_name": 1, "taxon_id": 1, "_id": 0}).exec(callback);
+}
+
+module.exports.getAllGenesBySpecies = (species, callback) => {
+    SpeciesGenes.find({'short_name': species}).limit(1000).exec(callback);
+}
