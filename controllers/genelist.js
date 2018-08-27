755fc25a88703a52d2cd8f7279b8ab7a6409031a
Xiaosong Huang <xhuang310@gatech.edu>
2018-08-27 13:09:58 -0700

    test

--- controllers/genelist.js
+++ controllers/genelist.js
@@ -165,7 +165,7 @@ router.get('/gene/:ptn',(req,res) => {
         if(!error){
             var $ = cheerio.load(html);
             //console.log(html);
-            var dir_annot_sec = $('table').get(1).html();
+            var dir_annot_sec = $('table').html();
             console.log(dir_annot_sec);
         }
     })
