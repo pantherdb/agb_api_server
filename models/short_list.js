050f336b5b5f4d60b8866751ab3beba7f55e883c
Xiaosong Huang <xhuang310@gatech.edu>
2018-09-25 07:34:39 -0700

    test

--- models/short_list.js
+++ models/short_list.js
@@ -22,7 +22,7 @@ module.exports.getListsBySpecies = (species, page, limit, callback) => {
     ShortGeneList.find({$or: [{'species': species}, {'species_long': species}]},{'_id':0,'species':0}).skip(limit*(page-1)).limit(limit).exec(callback);
 }
 module.exports.getGeneGains = (exspecies, anspecies,page, limit, callback) => {
-    ShortGeneList.find({$and: [{$not: {'ancestor_species': new RegExp(anspecies), 'pthr': 'NOT_AVAILABLE'}}, {$or: [{'species': exspecies}, {'species_long': exspecies}]}]}, {'_id':0,'species':0}).skip(limit*(page-1)).limit(limit).exec(callback);
+    ShortGeneList.find({$and: [{$ne: {'ancestor_species': new RegExp(anspecies), 'pthr': 'NOT_AVAILABLE'}}, {$or: [{'species': exspecies}, {'species_long': exspecies}]}]}, {'_id':0,'species':0}).skip(limit*(page-1)).limit(limit).exec(callback);
 }
 
 /* module.exports.getGeneGainsNum = (exspecies, anspecies, callback) => {
