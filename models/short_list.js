489d3c11eda6fe1c8c923019adad8fd13dd8b925
Xiaosong Huang <xhuang310@gatech.edu>
2018-10-09 11:01:54 -0700

    update short_list

--- models/short_list.js
+++ models/short_list.js
@@ -25,21 +25,21 @@ const ShortGenelistSchema = mongoose.Schema({
 }); */
 
 //const GeneList = module.exports = mongoose.model('genelists', GenelistSchema );
-const ShortGeneList = module.exports = mongoose.model('gene_one_proxy_lists', ShortGenelistSchema );
+const ShortGeneList = module.exports = mongoose.model('short_lists', ShortGenelistSchema );
 
 //const GeneListFlat = module.exports = mongoose.model('flat_genelists', GenelistFlatSchema );
 
 module.exports.getTotalGeneCountBySpecies = (species, callback) => {
-    ShortGeneList.find({$or: [{'species': species}, {'species_long': species}]}).count({}).exec(callback);
+    ShortGeneList.find({$or: [{'species_short': species}, {'species_long': species}]}).count({}).exec(callback);
 }
 module.exports.getListsBySpecies = (species, page, limit, callback) => {
-    ShortGeneList.find({$or: [{'species': species}, {'species_long': species}]},{'_id':0,'species':0, 'species_long':0, 'ancestor_species':0}).skip(limit*(page-1)).limit(limit).exec(callback);
+    ShortGeneList.find({$or: [{'species_short': species}, {'species_long': species}]},{'_id':0,'species':0, 'species_long':0, 'ancestor_species':0}).skip(limit*(page-1)).limit(limit).exec(callback);
 }
 module.exports.getGainedGenes = (anspecies, exspecies, page, limit, callback) => {
     ShortGeneList.find(
         {$and:[
             {'ancestor_species': {$not: new RegExp(anspecies)}, 'pthr': {$not: /NOT_AVAILABLE/}},
-            {$or: [{'species': exspecies}, {'species_long': exspecies}]}
+            {$or: [{'species_short': exspecies}, {'species_long': exspecies}]}
         ]}, 
         {'_id':0,'species':0, 'species_long':0, 'pthr':0, 'ancestor_species':0, 'proxy_gene':0}
     ).skip(limit*(page-1)).limit(limit).exec(callback);
