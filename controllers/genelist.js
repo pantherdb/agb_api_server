61591628cef10746b90bcf6324a343c38c56135b
Xiaosong Huang <xiaosonghuang@Xiaosongs-MacBook-Pro.local>
2020-03-12 16:50:54 -0700

    test

--- controllers/genelist.js
+++ controllers/genelist.js
@@ -319,6 +319,7 @@ router.get('/direct-inherited', (req,res) => {
             res.json({ success: false, message: `Failed to load all extant lists. Error: ${err}` });
         }
         else {
+            console.log(lists);
             var uniqueItems = [...new Set(lists)];
             //uniqueItems.map(gene=>gene.all_desendant_ptn_in_proxy_species = gene.all_desendant_ptn_in_proxy_species.split(','));
             var total = uniqueItems.length;
