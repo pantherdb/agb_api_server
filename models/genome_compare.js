d061c7cb8073330ce1f81560550bf795e7638d69
Xiaosong Huang <xiaosonghuang@Xiaosongs-MacBook-Pro.local>
2020-03-12 17:52:44 -0700

    test

--- models/genome_compare.js
+++ models/genome_compare.js
@@ -30,7 +30,7 @@ const mongoose = require('mongoose');
 	recipient_gene_ptn : {type: String, required:false},
 	duplication_node_ptn : {type: String, required:false},
 },{strict: false}); */
-const GenomeCompareSchema = mongoose.Schema({ any: Schema.Types.Mixed })
+const GenomeCompareSchema = mongoose.Schema({ any: {} })
 
 const GenomeCompare = module.exports = mongoose.model('genomeComparison', GenomeCompareSchema );
 
