2c5af2789c422a47478aa95bb03162c80d629200
Xiaosong Huang <xiaosonghuang@Xiaosongs-MacBook-Pro.local>
2020-03-12 18:58:29 -0700

    tt

--- models/genome_compare.js
+++ models/genome_compare.js
@@ -37,7 +37,7 @@ const GenomeCompare = module.exports = mongoose.model('genomeComparison', Genome
 
 
 module.exports.getDirectInheritedGenes = (species, callback) => {
-    GenomeCompare.find({}).exec(callback);
+    GenomeCompare.find().exec(callback);
     //GenomeCompare.find({'child_species_short': species, 'event': 'AncestralGene-DirectInheritanceFromOneGene'}).limit(20).exec(callback);
 }
 
