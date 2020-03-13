94d321b9d31e459f9d5596fd8911d93c10d8efeb
Xiaosong Huang <xiaosonghuang@Xiaosongs-MacBook-Pro.local>
2020-03-12 18:50:25 -0700

    test

--- controllers/genelist.js
+++ controllers/genelist.js
@@ -312,9 +312,10 @@ router.get('/gene-no-model/:exspecies', cache('2 hours'), (req, res) => {
     });
 });
 
-router.get('/direct-inherited', (req,res) => {
-    //var parspecies = req.params.parspecies;
-    GenomeCompare.getDirectInheritedGenes((err, lists) => {
+router.get('/direct-inherited/:species', (req,res) => {
+    var species = req.params.species;
+    //GenomeCompare.getDirectInheritedGenes((err, lists) => {
+    Species.getSpeciesDetail(species, (err, lists) => {
         if (err) {
             res.json({ success: false, message: `Failed to load all extant lists. Error: ${err}` });
         }
