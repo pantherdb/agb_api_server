c936a35fcc0c694e61938767341814b9fd5253c2
Xiaosong Huang <xiaosonghuang@Xiaosongs-MacBook-Pro.local>
2020-03-21 14:46:13 +0800

    test

--- models/gene_history.js
+++ models/gene_history.js
@@ -22,7 +22,7 @@ module.exports.getDirectInheritedGenes = (parspecies, chspecies, page, limit, ca
          {'event':'ExtantGene-DirectInheritanceFromOneGene'}
         ]}
         ]} */
-        {'parent_species_short': parspecies, 'child_species_short': chspecies, 'event':'AncestralGene-DirectInheritanceFromOneGene'}
+        {'parent_species_short': parspecies, 'child_species_short': chspecies}
         
         ).skip(limit*(page-1)).limit(20).exec(callback);
 	//console.log(parspecies);
